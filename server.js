d4e0b87286c23c954f4a3c1f88c6242a63791705
Mohamedaboda-bit <72769504+Mohamedaboda-bit@users.noreply.github.com>
2020-12-15 15:37:02 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const app = express();
-const bodyParser = require('body-Parser');
+const bodyParser = require('body-parser');
 // Start up an instance of app
 
 /* Middleware*/
